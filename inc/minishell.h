f2c0420d7db5b1190657c3605ce3da461381fe27
eduaserr <157403329+eduaserr@users.noreply.github.com>
2025-03-17 16:58:01 +0100

    update .h structs

--- inc/minishell.h
+++ inc/minishell.h
@@ -2,6 +2,33 @@
 #define MINISHELL_H
 
 #include "../lib/libft/libft.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <stdio.h>
+
+typedef struct s_fd
+{
+	int	in;
+	int	out;
+}	t_fd;
+
+typedef struct s_env //env structure
+{
+	char	**env;
+	char	**path;
+}	t_env;
+
+typedef struct s_shell
+{
+	char	**args;
+}	t_shell;
+
+typedef struct s_stack
+{
+	int		exit_status;
+	char	**user_input;
+	t_shell	*minishell;
+	t_env	*env;
+}	t_stack;
 
 #endif
