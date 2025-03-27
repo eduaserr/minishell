76b7d716bd37a7643f4c05fb9a2e86e916b02e73
eduaserr <157403329+eduaserr@users.noreply.github.com>
2025-03-27 18:47:51 +0100

    update split_env

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 16:24:27 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/03/26 20:45:19 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/03/27 17:15:33 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,22 @@ void	promp_input(t_shell *mshell)
 		exit(EXIT_SUCCESS);
 	}
 	ft_printf("%s\n", input);
-	ft_free_str(&input);
+	input = ft_free_str(&input);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_shell	*mshell;
-	//char	**split;
 
-	//(void)split;
-	//split = NULL;
 	(void)argc;
 	(void)argv;
 	mshell = NULL;
-	signal_function();
+	//signal_function();
 	mshell = init_mshell(mshell, envp); //init lstenv as stack
 	if (!mshell)
 		return (ft_error("init minishell"), 0);
-	while (1)
-		promp_input(mshell);//Ctrl + D signal se maneja con readline
+/* 	while (1)
+		promp_input(mshell);//Ctrl + D signal se maneja con readline */
 	printf("minishell :)\n");
 	ft_free_mshell(mshell);
 	return (0);
--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 13:12:54 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/03/26 20:47:55 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/03/27 17:51:00 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,14 @@ t_env	*split_env(t_env *new, char *env)
 	i = 0;
 	arr = NULL;
 	arr = ft_split(env, '=');
-	if (!arr || !*arr)
+	if (!arr || !arr[0] || !arr[1])
 		return (ft_error("split"), NULL);
-	ft_printf("%s\n", env);
-	ft_printmatrix(arr);
-	ft_printf("\n<-------->\n");
 	new->key = ft_strdup(arr[0]);
 	if (!new->key)
-		return (NULL);
+		return (ft_freematrix(arr), NULL);
 	new->value = ft_strdup(arr[1]);
 	if (!new->value)
-		return (NULL);
+		return (ft_freematrix(arr), NULL);
 	return (ft_freematrix(arr), new);
 }
 
@@ -38,11 +35,12 @@ static t_env	*create_env(char *env)
 {
 	t_env	*new;
 
-	(void)env;
 	new = (t_env *)malloc(sizeof(t_env));
 	if (!new)
 		return (NULL);
 	new = split_env(new, env);
+	if (!new)
+		return (NULL);
 	new->next = NULL;
 	new->prev = NULL;
 	return (new);
