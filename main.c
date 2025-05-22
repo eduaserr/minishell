5da12f57a8c25f16044b50cfe3a327a7829e61e7
eduaserr <157403329+eduaserr@users.noreply.github.com>
2025-05-22 19:09:05 +0200

    parse_cwd added, promp_input updated. Now it reflects the actual pwd

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eduaserr <eduaserr@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 16:24:27 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/05/22 05:01:16 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/05/22 19:00:31 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,43 @@ char	**ft_split_input(char *str)
 		return (ft_error("split error"), NULL);
 	return (input);
 }
+static char	*parse_pwd(t_shell *mshell, char *pwd)
+{
+	char	*home;
+	char	*tmp;
+
+	tmp = NULL;
+	home = NULL;
+	home = ft_getenv(mshell->lstenv, "HOME");
+	if (!home)
+		return (pwd);
+	if (ft_strncmp(pwd, home, ft_strlen(home)) == 0)
+	{
+		tmp = ft_substr(pwd, ft_strlen(home), ft_strlen(pwd) - ft_strlen(home));
+		pwd = ft_free_str(&pwd);
+		pwd = ft_strjoin("~", tmp);
+		tmp = ft_free_str(&tmp);
+	}
+	home = ft_free_str(&home);
+	return (pwd);
+}
 
 char	*promp_input(t_shell *mshell)
 {
 	char	*promp;
 	char	*input;
-	char	*cwd;
+	char	*pwd;
 
-	(void)cwd;
-	cwd = NULL;
+	pwd = NULL;
 	input = NULL;
 	promp = NULL;
-//	cwd = getcwd(NULL, 0);
-	promp = ft_getenv(mshell->lstenv, "USER");
-	promp = ft_strjoin_gnl(promp, "@mshell");
-//	promp = ft_strjoin_gnl(promp, cwd);
-	promp = ft_strjoin_gnl(promp, "$ ");
+	pwd = ft_getenv(mshell->lstenv, "PWD");		// /home/eduaserr/cursus/minishell
+	pwd = parse_pwd(mshell, pwd);
+	promp = ft_getenv(mshell->lstenv, "USER");	//eduaserr
+	promp = ft_strjoin_gnl(promp, "@mshell");	//eduaserr@mshell
+	promp = ft_strjoin_gnl(promp, pwd);			//eduaserr@mshell/home/eduaserr/cursus/minishell
+	pwd = ft_free_str(&pwd);
+	promp = ft_strjoin_gnl(promp, "$ ");		//eduaserr@mshell/home/eduaserr/cursus/minishell$ 
 	input = readline(promp);
 	promp = ft_free_str(&promp);
 	if (!input)
