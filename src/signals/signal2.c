aa109b7ef2ddd9a8a0cf255c74a6245c06535ed8
eduaserr <157403329+eduaserr@users.noreply.github.com>
2025-06-13 20:18:27 +0200

    signal heredoc add, not implemented

--- src/signals/signal2.c
+++ src/signals/signal2.c
@@ -6,13 +6,14 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 19:19:24 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/06/12 22:03:11 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/06/13 20:04:33 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
 //aún por añadir y comprobar funcionamiento
+/* heredoc signals */
 static void	handle_heredoc_sigint(int sig)
 {
 	g_signal_received = sig;
@@ -23,4 +24,18 @@ void	setup_heredoc_signals(void)
 {
 	signal(SIGINT, handle_heredoc_sigint);
 	signal(SIGQUIT, SIG_IGN);
+}
+
+/* exec signals */
+static void	sigquit_handler(int signal)
+{
+	(void)signal;
+	ft_putendl_fd("Quit (core dumped)", STDOUT_FILENO);
+}
+
+void	init_signal_handler_exec(void)
+{
+	g_signal_received = 0;
+	signal(SIGQUIT, sigquit_handler);
+	signal(SIGINT, SIG_IGN);
 }
