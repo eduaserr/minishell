b110979d1869b6800cf6bdfa3552f81b1fa18d83
Alejandro Amoros Rodriguez <aamoros-@c1r5s4.42malaga.com>
2025-07-02 18:20:40 +0200

    improved cd builtin

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
+/*   By: aamoros- <aamoros-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 13:24:48 by aamoros-          #+#    #+#             */
-/*   Updated: 2025/07/01 17:21:24 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/07/02 18:19:34 by aamoros-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ static int	update_env_pwd(t_shell *shell, char *oldpwd_val)
 		perror("minishell: cd: error retrieving current directory");
 		return (EXIT_FAILURE);
 	}
-	// ✅ Actualizar OLDPWD en la lista enlazada
 	if (oldpwd_val)
 	{
 		oldpwd_node = ft_getlstenv(shell->lstenv, "OLDPWD");
@@ -34,7 +33,6 @@ static int	update_env_pwd(t_shell *shell, char *oldpwd_val)
 		}
 		else
 		{
-			// Crear nuevo nodo OLDPWD
 			oldpwd_node = create_env("OLDPWD=");
 			if (oldpwd_node)
 			{
@@ -44,61 +42,53 @@ static int	update_env_pwd(t_shell *shell, char *oldpwd_val)
 			}
 		}
 	}
-    // ✅ Actualizar PWD en la lista enlazada
-    pwd_node = ft_getlstenv(shell->lstenv, "PWD");
-    if (pwd_node)
-    {
-        free(pwd_node->value);
-        pwd_node->value = ft_strdup(cwd);
-    }
-    else
-    {
-        // Crear nuevo nodo PWD
-        pwd_node = create_env("PWD=");
-        if (pwd_node)
-        {
-            free(pwd_node->value);
-            pwd_node->value = ft_strdup(cwd);
-            addlast_node(&shell->lstenv, pwd_node);
-        }
-    }
-    
-    // ✅ Sincronizar con el array env
-    sync_env_array(shell);
-    
-    return (EXIT_SUCCESS);
+	pwd_node = ft_getlstenv(shell->lstenv, "PWD");
+	if (pwd_node)
+	{
+		free(pwd_node->value);
+		pwd_node->value = ft_strdup(cwd);
+	}
+	else
+	{
+		pwd_node = create_env("PWD=");
+		if (pwd_node)
+		{
+			free(pwd_node->value);
+			pwd_node->value = ft_strdup(cwd);
+			addlast_node(&shell->lstenv, pwd_node);
+		}
+	}
+	sync_env_array(shell);
+	return (EXIT_SUCCESS);
 }
 
 static int	cd_to_oldpwd(t_shell *shell)
 {
-    char	*path_to_oldpwd;
-    char	*current_pwd;
+	char	*path_to_oldpwd;
+	char	*current_pwd;
 
-    current_pwd = ft_getenv(shell->lstenv, "PWD");  // ✅ Sintaxis correcta
-    if (!current_pwd)
-        return (EXIT_FAILURE);
-    
-    path_to_oldpwd = ft_getenv(shell->lstenv, "OLDPWD");  // ✅ Sintaxis correcta
-    if (!path_to_oldpwd)
-    {
-        ft_putendl_fd("minishell: cd: OLDPWD not set", 2);
-        free(current_pwd);
-        return (EXIT_FAILURE);
-    }
-    
-    if (chdir(path_to_oldpwd) == -1)
-    {
-        perror("minishell: cd");
-        free(current_pwd);
-        free(path_to_oldpwd);
-        return (EXIT_FAILURE);
-    }
-    
-    ft_putendl_fd(path_to_oldpwd, 1);
-    update_env_pwd(shell, current_pwd);
-    free(current_pwd);
-    free(path_to_oldpwd);
-    return (EXIT_SUCCESS);
+	current_pwd = ft_getenv(shell->lstenv, "PWD");
+	if (!current_pwd)
+		return (EXIT_FAILURE);
+	path_to_oldpwd = ft_getenv(shell->lstenv, "OLDPWD");
+	if (!path_to_oldpwd)
+	{
+		ft_putendl_fd("minishell: cd: OLDPWD not set", 2);
+		free(current_pwd);
+		return (EXIT_FAILURE);
+	}
+	if (chdir(path_to_oldpwd) == -1)
+	{
+		perror("minishell: cd");
+		free(current_pwd);
+		free(path_to_oldpwd);
+		return (EXIT_FAILURE);
+	}
+	ft_putendl_fd(path_to_oldpwd, 1);
+	update_env_pwd(shell, current_pwd);
+	free(current_pwd);
+	free(path_to_oldpwd);
+	return (EXIT_SUCCESS);
 }
 
 static int	cd_to_home(t_shell *shell)
@@ -139,10 +129,27 @@ int	ft_strcmp(const char *s1, const char *s2)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
+static int	count_args(char **cmd)
+{
+	int	count;
+
+	count = 0;
+	while (cmd[count])
+		count++;
+	return (count);
+}
+
 int	builtin_cd(t_shell *shell, char **cmd)
 {
 	char	*oldpwd_val;
+	int		arg_count;
 
+	arg_count = count_args(cmd);
+	if (arg_count > 2)
+	{
+		ft_putendl_fd("minishell: cd: too many arguments", 2);
+		return (EXIT_FAILURE);
+	}
 	if (!cmd[1] || ft_strcmp(cmd[1], "~") == 0)
 		return (cd_to_home(shell));
 	if (ft_strcmp(cmd[1], "-") == 0)
