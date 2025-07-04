c7765e38a64bd1bd254b6ee819f4bd9d1b221a02
eduaserr <157403329+eduaserr@users.noreply.github.com>
2025-07-04 19:18:53 +0200

    main heredoc

--- main.c
+++ main.c
@@ -19,12 +19,117 @@ void	update_shell(t_shell **mshell)
 	ft_free_mshell(mshell);
 }
 
+
+void	end_of_heredoc(char *line, char *filename, t_redir *rd)
+{
+	if (line)
+	{
+		free(line);
+		free(rd->file);
+		rd->file = filename;
+	}
+	else if (g_signal_received == SIGINT)
+	{
+		unlink(filename);
+		free(filename);
+		g_signal_received = 0;
+	}
+	else
+	{
+		free(rd->file);
+		rd->file = filename;
+		ft_putendl_fd("minishell: heredoc ended without input", STDERR_FILENO);
+	}
+}
+
+void	expand_heredoc(t_shell *shell, char **pline)
+{
+	char	*line;
+	int		i;
+	
+	line = *pline;
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '$' && line[i + 1] && line[i + 1] != '?')
+			dollar_qu(pline, shell->last_exit_status, i);
+		else if (line[i] == '$')
+			dollar_expand(pline, shell->lstenv, i);
+		line = *pline;
+		i++;
+	}
+}
+
+void	create_heredoc_file(t_shell *shell, t_redir *rd, char *n)
+{
+	char	*filename;
+	char	*line;
+	int		fd;
+
+	filename = ft_strjoin("/tmp/heredoc_", n);
+	free(n);
+	if (!filename)
+	{
+		ft_error("minishell: Memory allocation failed for heredoc filename.");
+		ft_exit(&shell);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+	{
+		ft_error("minishell: Failed to open heredoc file.");
+		free(filename);
+		ft_exit(&shell);
+	}
+	line = readline("> ");
+	while (line && ft_strcmp(line, rd->file) && g_signal_received != SIGINT)
+	{
+		if (line[0] == '\0')
+		{
+			free(line);
+			line = ft_strdup("\n");
+		}
+		expand_heredoc(shell, &line);
+		ft_putendl_fd(line, fd);
+		free(line);
+		line = readline("> ");
+	}
+	close(fd);
+	end_of_heredoc(line, filename, rd);
+}
+
+void	create_heredocs(t_shell *shell)
+{
+	t_cmd	*cmd;
+	t_redir	*rd;
+	int		n;
+
+	
+	setup_heredoc_signals();
+	cmd = shell->commands;
+	while (cmd && g_signal_received != SIGINT)
+	{
+		n = 0;
+		rd = cmd->rd;
+		while (rd)
+		{
+			if (rd->type == HEREDOC)
+				create_heredoc_file(shell, rd, ft_itoa(n++));
+			rd = rd->next;
+		}
+		cmd = cmd->next;
+	}
+}
+
 static void	execute_command_pipeline(t_shell *shell)
 {
 	int	pid;
 	int	status;
 
 	sync_env_array(shell);
+	create_heredocs(shell);
+	if (g_signal_received == SIGINT)
+		return ;
+	handle_exec_sigquit();
 	if (execute_parent_builtin(shell->commands->args, shell))
 		return ;
 	pid = fork();
@@ -47,6 +152,25 @@ static void	execute_command_pipeline(t_shell *shell)
 	}
 }
 
+void	remove_heredocs(t_shell *shell)
+{
+	t_cmd	*cmd;
+	t_redir	*redir;
+
+	cmd = shell->commands;
+	while (cmd)
+	{
+		redir = cmd->rd;
+		while (redir)
+		{
+			if (redir->type == HEREDOC && redir->file)
+				unlink(redir->file);
+			redir = redir->next;
+		}
+		cmd = cmd->next;
+	}
+}
+
 static void	process_command_line(t_shell *shell)
 {
 	char	*input;
@@ -60,10 +184,12 @@ static void	process_command_line(t_shell *shell)
 	if (input[0] != '\0')
 	{
 		parse_input(&shell, ft_strdup(input));
-		//ft_printtkn(shell->tkn);
-		//ft_printcmd(shell->commands);
 		if (shell->commands)
+		{
 			execute_command_pipeline(shell);
+			remove_heredocs(shell);
+			signal_function();
+		}
 		update_shell(&shell);
 	}
 	free(input);
