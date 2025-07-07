7fb1f41c96b641e1e3ea4053d294829a8749354a
eduaserr <eduaserr@github.com>
2025-07-08 00:37:57 +0200

    cmd functions reorganizated and refactorized. normi

--- src/command/cmd.c
+++ src/command/cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cmd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aamoros- <aamoros-@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: eduaserr <eduaserr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 21:16:14 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/07/07 15:30:22 by aamoros-         ###   ########.fr       */
+/*   Updated: 2025/07/08 00:37:16 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,7 @@ static char	*parse_cmd(char *input, int start, int pipe)
 	return (tmp);
 }
 
-static t_cmd	*ft_nodecmd(t_shell *shell, t_cmd *cmd, char *input, int start,
-		int pipe)
+static t_cmd	*ft_nodecmd(t_shell *shell, t_cmd *cmd, char *input, int *ix)
 {
 	t_cmd	*new;
 
@@ -41,7 +40,7 @@ static t_cmd	*ft_nodecmd(t_shell *shell, t_cmd *cmd, char *input, int start,
 	new = create_cmd(new);
 	if (!new)
 		return (NULL);
-	new->cmd = parse_cmd(input, start, pipe);
+	new->cmd = parse_cmd(input, ix[0], ix[1]);
 	if (!new->cmd)
 		return (free(new), ft_free_cmd(&cmd), ft_error("trim input"), NULL);
 	new->rd = redir_node(shell, shell->tkn, new->rd, shell->cmd_count);
@@ -49,30 +48,52 @@ static t_cmd	*ft_nodecmd(t_shell *shell, t_cmd *cmd, char *input, int start,
 	return (cmd);
 }
 
-t_cmd	*get_cmd(t_shell *mshell, t_cmd *cmd, char *input, int is_pipe)
+static int	find_next_pipe(char *input, int start)
 {
 	int	i;
 
-	i = -1;
-	mshell->cmd_count = 0;
-	while (input[++i])
+	i = start;
+	while (input[i])
 	{
 		if (skip_quoted(input, &i))
 			continue ;
 		if (input[i] == '|')
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
+static t_cmd	*process_pipe_segment(t_shell *mshell, t_cmd *cmd, 
+		char *input, int *ix)
+{
+	cmd = ft_nodecmd(mshell, cmd, input, ix);
+	if (!cmd)
+		return (NULL);
+	mshell->cmd_count++;
+	return (cmd);
+}
+
+t_cmd	*get_cmd(t_shell *mshell, t_cmd *cmd, char *input, int is_pipe)
+{
+	int	pos[2];
+
+	pos[0] = is_pipe;
+	while (input[pos[0]])
+	{
+		pos[1] = find_next_pipe(input, pos[0]);
+		if (input[pos[1]] == '|')
 		{
-			cmd = ft_nodecmd(mshell, cmd, input, is_pipe, i);
+			cmd = process_pipe_segment(mshell, cmd, input, pos);
 			if (!cmd)
 				return (ft_error("Parse command"), NULL);
-			is_pipe = i + 1;
-			mshell->cmd_count++;
+			pos[0] = pos[1] + 1;
 		}
+		else
+			break ;
 	}
-	if (input[i] == '\0')
-	{
-		cmd = ft_nodecmd(mshell, cmd, input, is_pipe, i);
-		if (!cmd)
-			return (ft_error("Parse last command"), NULL);
-	}
+	cmd = ft_nodecmd(mshell, cmd, input, pos);
+	if (!cmd)
+		return (ft_error("Parse last command"), NULL);
 	return (cmd);
 }
